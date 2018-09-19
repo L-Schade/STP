27c69a5c19b16c54ae12dd6f679d57e80cb8320e
L-Schade <lischade@uni-bremen.de>
2018-09-19 22:03:36 +0200

    kleine veraenderungen, durch tests

--- Motoransteuerung/Microcontroller/GPIO.py
+++ Motoransteuerung/Microcontroller/GPIO.py
@@ -1,23 +1,27 @@
 import RPi.GPIO as GPIO
 from time import sleep
  
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
  
-Motor1A = 16
-Motor1B = 18
-Motor1E = 22
+Motor1A = 4
+Motor1B = 23
+Motor1E = 24
  
 GPIO.setup(Motor1A,GPIO.OUT)
 GPIO.setup(Motor1B,GPIO.OUT)
 GPIO.setup(Motor1E,GPIO.OUT)
  
 print "Turning motor on"
+# for x in range(0,5,1):
 GPIO.output(Motor1A,GPIO.HIGH)
 GPIO.output(Motor1B,GPIO.LOW)
 GPIO.output(Motor1E,GPIO.HIGH)
- 
 sleep(2)
- 
+
+GPIO.output(Motor1A,GPIO.HIGH)
+GPIO.output(Motor1B,GPIO.LOW)
+GPIO.output(Motor1E,GPIO.LOW)
+
 print "Stopping motor"
 GPIO.output(Motor1E,GPIO.LOW)
  
