e69b2c946eb406445826cd21ff327ce01805583e
L-Schade <lischade@uni-bremen.de>
2018-09-21 10:45:37 +0200

    sudo in python programm ausfuehren

--- Motoransteuerung/Mikrocontroller/pwmTest.py
+++ Motoransteuerung/Mikrocontroller/pwmTest.py
@@ -1,17 +1,27 @@
 import wavePWM
 import pigpio
 import time
+import os
+from subprocess import Popen, PIPE
+
+
+sudo_password = ''
+command = 'pigpiod'.split()
+
+p = Popen(['sudo', '-S'] + command, stdin=PIPE, stderr=PIPE,
+          universal_newlines=True)
+sudo_prompt = p.communicate(sudo_password + '\n')[1]
 
 pi=pigpio.pi()
 pwm=wavePWM.PWM(pi)
 
-pwm.set_frequency(8)
+pwm.set_frequency(50000)
 print(pwm.frequency)
 print(pwm.get_cycle_length())
 
-pwm.set_pulse_start_and_length_in_fraction(4,0,1.0/1.75)
-pwm.set_pulse_start_and_length_in_fraction(23,1.0/6,1.0/1.75)
-pwm.set_pulse_start_and_length_in_fraction(24,1.0/3,1.0/1.75)
+pwm.set_pulse_start_and_length_in_fraction(4,0,2)
+pwm.set_pulse_start_and_length_in_fraction(23,0.33,2)
+pwm.set_pulse_start_and_length_in_fraction(24,0.66,2)
 
 ##pwm.set_pulse_start_and_length_in_micros(4,0,5000)
 ##pwm.set_pulse_start_and_length_in_micros(23,50,5)
@@ -20,5 +30,5 @@ pwm.set_pulse_start_and_length_in_fraction(24,1.0/3,1.0/1.75)
 
 pwm.update()
 
-time.sleep(0.05)
+time.sleep(2)
 pwm.cancel()
