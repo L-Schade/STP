dcea8a182ff91e1a2bb5f39ebcd898dac915a7a3
lischade <lischade@uni-bremen.de>
2018-09-24 23:14:50 +0200

    update motor_controllG

--- GUI/motor_controlG.py
+++ GUI/motor_controlG.py
@@ -13,13 +13,19 @@ coil_C_pin = 24 # blau
 ind = 0
 # half-steps
 Seq = list(range(0, 6))
-Seq[0] = [1,1,0]
-Seq[1] = [1,0,0]
-Seq[2] = [1,0,1]
-Seq[3] = [0,0,1]
-Seq[4] = [0,1,1]
-Seq[5] = [0,1,0]
-Seq[5] = [0,1,0]
+Seq[0] = [1,0,0]
+Seq[1] = [1,1,0]
+Seq[2] = [0,1,0]
+Seq[3] = [0,1,1]
+Seq[4] = [0,0,1]
+Seq[5] = [1,0,1]
+##Seq[0] = [1,1,0]
+##Seq[1] = [1,0,0]
+##Seq[2] = [1,0,1]
+##Seq[3] = [0,0,1]
+##Seq[4] = [0,1,1]
+##Seq[5] = [0,1,0]
+
 # full-steps
 ##Seq = list(range(0, 3))
 ##Seq[0] = [1,0,0]
@@ -82,16 +88,23 @@ def move(x_angle, y_angle, delay):
     else:
         backwards((float(delay) / 1000.0), int(steps))
 
+
+def angle_to_steps(angle):
+    angle_per_step = 4
+    steps = float(angle) / float(angle_per_step)
+
+    return steps
+    
               
 if __name__ == '__main__':
     try:
         delay = raw_input("Zeitverzoegerung (ms)?")
         while True:
-            ##        delay = raw_input("Zeitverzoegerung (ms)?")
+##            delay = raw_input("Zeitverzoegerung (ms)?") 
             steps = raw_input("Wie viele Schritte vorwaerts? ")
-            forward((float(delay) / 10.0),int(steps))
+            forward((int(delay)/ 1000.0), int(steps))
             steps = raw_input("Wie viele Schritte rueckwaerts? ")
-            backwards((float(delay) / 10.0), int(steps))
+            backwards((int(delay)/ 1000.0), int(steps))
     except KeyboardInterrupt:
         GPIO.cleanup()
 
